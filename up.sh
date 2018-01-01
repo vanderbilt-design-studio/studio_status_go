6f4b0d5944313d05605e29ec2d8fa35cafaf7d0c
Sameer Puri <sameer.puri@vanderbilt.edu>
2018-01-01 11:04:01 -0500

    Comment on up.sh

--- up.sh
+++ up.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+# Bash script to keep the studio status program running, even in the event of crashes.
+
 cd /home/pi/go/src/github.com/vanderbilt-design-studio/studio_status_go
+
 while :
 do 
-	./studio_status_go &
-	sleep 20
-	kill %1
-	./studio_status_go 1>>crashes.log 2>>crashes.log
-	date >> crashes.log
+	./studio_status_go & # Start the program and send it to the background
+	sleep 20             # Sleep for 20s
+	kill %1              # Kill it
+	./studio_status_go 1>>crashes.log 2>>crashes.log # Start it again & log all output
+	date >> crashes.log  # Log crash times for debugging
 done
 
