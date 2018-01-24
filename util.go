e37d8a576080f6c14e44b712bdc0b5bd5194ff2f
Sameer <spuri4096@gmail.com>
2018-01-24 10:53:17 -0600

    Use a mutex for serial acquiring of arduinos and close serial ports properly

--- util.go
+++ util.go
@@ -3,24 +3,30 @@ package main
 import (
 	"fmt"
 	"github.com/tarm/serial"
+	"sync"
 )
 
-func AcquireArduinoUID(uid byte) *serial.Port {
-	for i := range []int{0, 1, 2} {
-		serialConf := &serial.Config{Name: fmt.Sprint("/dev/ttyACM", i), Baud: 9600}
-		serialPort, err := serial.OpenPort(serialConf) // Try to open a serial port.
-		if err == nil {
-			serialPort.Write([]byte{identReq})
-			buf := make([]byte, 1)
-			serialPort.Read(buf)
-			if buf[0] == uid {
-				return serialPort
+var AcquireArduinoUID = func() func(byte) *serial.Port {
+	mtx := sync.Mutex{}
+	return func(uid byte) *serial.Port {
+		mtx.Lock()
+		defer mtx.Unlock()
+		for i := range []int{0, 1, 2} {
+			serialConf := &serial.Config{Name: fmt.Sprint("/dev/ttyACM", i), Baud: 9600}
+			serialPort, err := serial.OpenPort(serialConf) // Try to open a serial port.
+			if err == nil {
+				serialPort.Write([]byte{identReq})
+				buf := make([]byte, 1)
+				serialPort.Read(buf)
+				serialPort.Close()
+				if buf[0] == uid {
+					return serialPort
+				}
+				fmt.Println("Wrong port for", uid)
+			} else {
+				fmt.Println("Failed to acquire serial port:", uid, err)
 			}
-			serialPort.Close()
-			fmt.Println("Wrong port for", uid)
-		} else {
-			fmt.Println("Failed to acquire serial port:", uid, err)
 		}
+		return nil
 	}
-	return nil
-}
+}()
