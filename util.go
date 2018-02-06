ff9642f88f41aa7562e9683c3ef535e1e7568042
Sameer <spuri4096@gmail.com>
2018-02-06 11:28:13 -0600

    err == nil...

--- util.go
+++ util.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/tarm/serial"
 	"sync"
-	"time"
 )
 
 var AcquireArduinoUID = func() func(byte) *serial.Port {
@@ -20,15 +19,14 @@ var AcquireArduinoUID = func() func(byte) *serial.Port {
 			if isAcquired {
 				continue
 			}
-			serialConf := &serial.Config{Name: fmt.Sprint("/dev/ttyACM", port), Baud: 9600, ReadTimeout: time.Second * 5}
+			serialConf := &serial.Config{Name: fmt.Sprint("/dev/ttyACM", port), Baud: 9600}
 			serialPort, err := serial.OpenPort(serialConf) // Try to open a serial port.
 			if err == nil {
 				serialPort.Write([]byte{identReq})
 				serialPort.Flush()
 				buf := make([]byte, 1)
 				count, err := serialPort.Read(buf)
-				if buf[0] == uid && err != nil {
-					fmt.Println("Acquired", uid)
+				if buf[0] == uid && err == nil {
 					acquiredMap[port] = true
 					return serialPort
 				}
@@ -38,13 +36,14 @@ var AcquireArduinoUID = func() func(byte) *serial.Port {
 				} else if count != 1 {
 					fmt.Println("Reading from Arduino timed out")
 				} else {
-					fmt.Println("Wrong port for", uid)
+					fmt.Println("Wrong port for", uid, "got", buf[0])
 				}
 			} else {
 				fmt.Println("Failed to acquire serial port", uid, ":", err)
 				acquiredMap[port] = true
 			}
 		}
+		fmt.Println("No port found for", uid)
 		return nil
 	}
 }()
