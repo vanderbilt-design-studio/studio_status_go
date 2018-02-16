7ce805ced18c2719f10efc868be1cedddcd22396
Sameer <spuri4096@gmail.com>
2018-02-15 23:01:43 -0600

    Update shift schedule, generalize up.sh for ArchlinuxArm

--- up.sh
+++ up.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 # Bash script to keep the studio status program running, even in the event of crashes.
-export GOPATH=/home/pi/go
-cd /home/pi/go/src/github.com/vanderbilt-design-studio/studio_status_go
+cd $HOME/go/src/github.com/vanderbilt-design-studio/studio_status_go
+export GOPATH=$HOME/go
 git pull
 go build
 while :
 do
-	(sleep 15; ./studio_status_go) & # Wait 15s, start program and send to background
-	sleep 30             # Sleep for 30s
-	kill %1              # Kill it
-	./studio_status_go 1>>crashes.log 2>>crashes.log # Start it again & log all output
+	./studio_status_go 2>>crashes.log # Start it & log all errors
 	date >> crashes.log  # Append crash times to log for debugging
 done
